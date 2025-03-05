a560de503a0bcb85ff3a7b5779d0930aa35c8962
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-03-05 23:47:00 +0700

    update SeatBooking page

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
 export const HEADER_HEIGHT = 100;
 export const HEIGHT_BANNER = 200;
 export const MIN_HEIGHT_FOOTER = 300;
+
+export const chairs = {
+    a: Array.from({ length: 22 }, (_, i) => `a${i + 1}`),
+    b: Array.from({ length: 22 }, (_, i) => `b${i + 1}`),
+    c: Array.from({ length: 22 }, (_, i) => `c${i + 1}`),
+    d: Array.from({ length: 22 }, (_, i) => `d${i + 1}`),
+};
