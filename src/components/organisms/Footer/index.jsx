92764223cd332145f76c0ccdcb6646258d875735
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-03-06 00:17:23 +0700

    footer

--- src/components/organisms/Footer/index.jsx
+++ src/components/organisms/Footer/index.jsx
@@ -1,9 +1,32 @@
-import { MIN_HEIGHT_FOOTER } from "../../../constants";
+import ContainerWapper from '../../templates/ContainerWapper';
+import p1 from '../../../../public/images/content/p1.jpg';
+import p2 from '../../../../public/images/content/p2.jpg';
+import p3 from '../../../../public/images/content/p3.jpg';
+import p4 from '../../../../public/images/content/p4.jpg';
+import p5 from '../../../../public/images/content/p5.jpg';
+import p6 from '../../../../public/images/content/p6.jpg';
 
 const Footer = () => {
     return (
-        <div className="bg-dark" style={{ height: `${MIN_HEIGHT_FOOTER}px` }}>
-            Footer
+        <div className="w-[100%] shadow">
+            <div className="pt-[90px] pb-[80px] w-[100%] bg-white">
+                <ContainerWapper>
+                    <div className="flex flex-col items-center">
+                        <p className="text-[32px] font-[500] text-center cursor-pointer mb-[16px]">Our Patner’s</p>
+                        <div className="flex justify-center items-center w-[200px] mb-[60px]">
+                            <div className="w-[40%] h-[2px] bg-[#ccc]"></div>
+                            <div className="w-[20%] h-[4px] bg-[#f44343]"></div>
+                            <div className="w-[40%] h-[2px] bg-[#ccc]"></div>
+                        </div>
+
+                        <div className="flex justify-center items-center gap-[20px]">
+                            {[p1, p2, p3, p4, p5, p6].map((item, index) => {
+                                return <img alt="" src={item} key={index} className="w-[170px]" />;
+                            })}
+                        </div>
+                    </div>
+                </ContainerWapper>
+            </div>
         </div>
     );
 };
--- src/components/templates/MainTemplate.jsx
+++ src/components/templates/MainTemplate.jsx
@@ -1,5 +1,5 @@
 import Banner from '../organisms/Banner';
-// import Footer from "../organisms/Footer";
+import Footer from '../organisms/Footer';
 import WithHeaderTemplate from './WithHeaderTemplate';
 
 // eslint-disable-next-line react/prop-types
@@ -8,8 +8,8 @@ const MainTemplate = ({ children }) => {
         <WithHeaderTemplate>
             <>
                 <Banner />
-                {children}
-                {/* <Footer /> */}
+                <div className="bg-[#f6f6f6] pb-[60px]">{children}</div>
+                <Footer />
             </>
         </WithHeaderTemplate>
     );
