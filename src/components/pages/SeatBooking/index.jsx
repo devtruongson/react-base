efd4f3a703b73d85018888f48e2e406fb6e5563b
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-03-18 23:25:17 +0700

    update

--- src/components/pages/SeatBooking/index.jsx
+++ src/components/pages/SeatBooking/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Popover } from 'antd';
-import { useMemo, useState } from 'react';
+import { Fragment, useMemo, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import screen from '../../../../public/images/content/screen.png';
 import { formatVND } from '../../../helpers/formatVND';
@@ -8,14 +8,48 @@ import { routes } from '../../../routes';
 import ContainerWapper from '../../templates/ContainerWapper';
 
 const data = {
-    film: {
-        id: 1,
-        name: 'Aquaman',
-        nation: 'English',
-        duration: '2:30',
-    },
     booked: ['a3', 'a2', 'd1', 'd6'],
     price: 100000,
+};
+const film = {
+    id: 1,
+    name: 'Aquaman',
+    trailer_url: 'https://www.youtube.com/embed/d_S6HyolN_w',
+    categories: [
+        {
+            id: 1,
+            name: 'ACTION',
+        },
+        {
+            id: 22,
+            name: 'Adventure',
+        },
+        {
+            id: 3,
+            name: 'Fantasy',
+        },
+    ],
+    graphics: [
+        {
+            id: 1,
+            name: '2D',
+        },
+        {
+            id: 2,
+            name: '3D',
+        },
+        {
+            id: 3,
+            name: '4D',
+        },
+    ],
+    languages: ['ENGLISH', 'HINDI', 'TAMIL'],
+    duration: '2:23', // thoi luong
+    date: '2025/01/01',
+    like: 85,
+    votes: 52291,
+    rate: 4.5,
+    banners: ['', ''],
 };
 
 const SeatBooking = () => {
@@ -80,52 +114,153 @@ const SeatBooking = () => {
                 </ContainerWapper>
             </div>
 
-            <div className="bg-[#333545] pt-[40px] overflow-auto">
+            <div className="pt-[40px] overflow-auto">
                 <ContainerWapper>
-                    <div className="lg:w-[100%] w-[1300px]">
-                        <p className="text-center text-[16px] uppercase mb-[40px]">Ariesplex SL Cinemas</p>
-                        <img src={screen} alt="screen" className="mb-[40px]" />
-                        <div className="w-[100%] flex justify-between items-center">
-                            <div className="h-[2px] bg-[#ffffff61] w-[42%]"></div>
-                            <p className="uppercase text-[16px] py-[40px]">Economy</p>
-                            <div className="h-[2px] bg-[#ffffff61] w-[42%]"></div>
-                        </div>
-
-                        <div className="w-[100%] py-[30px] sm:px-[0px] px-[10px]">
-                            <ListChair
-                                target={'d'}
-                                booked={data?.booked}
-                                booking={booking}
-                                setBooking={setBooking}
-                                price={data?.price}
-                            />
-                        </div>
-
-                        <div className="w-[100%] flex justify-between items-center mb-[20px]">
-                            <div className="h-[2px] bg-[#ffffff61] w-[35%]"></div>
-                            <p className="uppercase text-[16px] py-[40px]">Executive + 3d glasss - rs 190.00</p>
-                            <div className="h-[2px] bg-[#ffffff61] w-[35%]"></div>
-                        </div>
-
-                        <div className="flex flex-col items-center gap-[40px] mb-[60px] sm:px-[0px] px-[10px]">
-                            {['c', 'b', 'a'].map((item, index) => {
-                                return (
-                                    <div className="w-[100%]" key={index}>
-                                        <ListChair
-                                            target={item}
-                                            booked={data?.booked}
-                                            booking={booking}
-                                            setBooking={setBooking}
-                                            price={data?.price}
-                                        />
+                    <div className="lg:w-[100%] w-[1300px] flex justify-between items-start">
+                        <div className="w-[45%]">
+                            <p className="text-center text-[16px] uppercase mb-[40px]">Ariesplex SL Cinemas</p>
+                            <div className="relative flex justify-center items-center">
+                                <img src={screen} alt="screen" className="mb-[40px]" />
+                                <p className="absolute uppercase text-[#000] text-[20px] font-[600]">màn hình chiếu</p>
+                            </div>
+                            <div className="flex flex-col items-center gap-[40px] mb-[60px] sm:px-[0px] px-[10px]">
+                                {['a', 'b', 'c', 'd', 'e', 'f'].map((item, index) => {
+                                    return (
+                                        <div className="w-[100%]" key={index}>
+                                            <ListChair
+                                                target={item}
+                                                booked={data?.booked}
+                                                booking={booking}
+                                                setBooking={setBooking}
+                                                price={data?.price}
+                                                type={[0, 1, 2].includes(index) ? 'nomal' : 'vip'}
+                                            />
+                                        </div>
+                                    );
+                                })}
+                                <div className="w-[100%]">
+                                    <div className="flex justify-center items-center gap-[50px]">
+                                        <p className="uppercase font-[500] text-[20px]">H</p>
+                                        <div className="flex justify-start items-center gap-[10px]">
+                                            {[
+                                                'H1 H2',
+                                                'H3 H4',
+                                                'H5 H6',
+                                                'H7 H8',
+                                                'H9 H10',
+                                                'H12 H11',
+                                                'H13 H14',
+                                                'H15 H16',
+                                            ].map((item) => {
+                                                return (
+                                                    <Chair
+                                                        name={item}
+                                                        key={item}
+                                                        booked={data?.booked}
+                                                        booking={booking}
+                                                        setBooking={setBooking}
+                                                        price={formatVND(data?.price)}
+                                                        isDoubleChair
+                                                    />
+                                                );
+                                            })}
+                                        </div>
                                     </div>
-                                );
-                            })}
+                                </div>
+                            </div>
+                            <div className="flex justify-center items-center mb-[40px]">
+                                <div className="grid grid-cols-3 gap-[24px]">
+                                    {[
+                                        { bg: '#1d59a2', label: 'Ghế bạn đang chọn' },
+                                        { bg: '#53b5fc', label: 'Ghế đang được giữ' },
+                                        { bg: '#babac1', label: 'Ghế đã đặt' },
+                                        { bg: '#7331d6', label: 'Ghế thường' },
+                                        { bg: '#f14052', label: 'Ghế VIP' },
+                                        { bg: '#dd3998', label: 'Ghế Sweetbox' },
+                                    ].map((item, index) => {
+                                        return (
+                                            <div className="flex justify-start items-center gap-[10px]" key={index}>
+                                                <div
+                                                    className="w-[40px] h-[40px]"
+                                                    style={{ background: item.bg }}
+                                                ></div>
+                                                <p className="text-[#000]">{item.label}</p>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+
+                            <div className="w-[100%] h-[6px] flex justify-center items-center mb-[60px]">
+                                <div className="w-[100%] h-[100%] bg-[#ff4444]"></div>
+                            </div>
+
+                            <div className="flex justify-between items-stretch text-[#000] w-[100%]">
+                                <div className="">Ghế thường</div>
+                                <div className="flex flex-col items-center">
+                                    <p>Ghế VIP</p>
+                                    <p></p>
+                                </div>
+                                <div className="">Ghế Sweetbox</div>
+                                <div className="flex flex-col items-center">
+                                    <p>Tổng tiền</p>
+                                    <p>1.200.000</p>
+                                </div>
+                                <div className="flex flex-col justify-end items-end">
+                                    <p>Thời gian Còn lại</p>
+                                    <p>3:00</p>
+                                </div>
+                            </div>
                         </div>
 
-                        <div className="w-[100%] h-[6px] flex justify-center items-center mb-[60px]">
-                            <div className="w-[90%] h-[100%] bg-[#ff4444]"></div>
-                            <div className="w-[10%] h-[100%] bg-white"></div>
+                        {/* detail movie */}
+                        <div className="w-[25%] text-[#000]">
+                            <div className="flex justify-start gap-[20px] mb-[40px]">
+                                <img
+                                    src="http://filmgo.io.vn/images/movies/AdZEpvwQxlHk.jpg"
+                                    alt=""
+                                    className="w-[150px] object-cover"
+                                />
+                                <div className="">
+                                    <p className="text-[#ff4444] text-[24px] font-[600]">{film.name}</p>
+                                    <p></p>
+                                </div>
+                            </div>
+
+                            <div className="">
+                                {[
+                                    {
+                                        icon: 'bi bi-film',
+                                        label: 'Rạp chiếu',
+                                        value: film.categories.map((item) => item.name).join(', '),
+                                    },
+                                    { icon: 'bi bi-stopwatch', label: 'Thời lượng', value: film.duration },
+                                    { icon: 'bi bi-film', label: 'Rạp chiếu', value: 'Rap ABC' },
+                                    { icon: 'bi bi-film', label: 'Ngày chiếu', value: film.date },
+                                    { icon: 'bi bi-clock', label: 'Giờ chiếu', value: 'Rap ABC' },
+                                    { icon: 'bi bi-tv', label: 'Phòng chiếu', value: 'Rap ABC' },
+                                    { icon: 'bi bi-film', label: 'Ghế', value: 'Rap ABC' },
+                                ].map((item, index) => {
+                                    return (
+                                        <Fragment key={index}>
+                                            <div className="flex justify-between mb-[16px]">
+                                                <div className="flex justify-start gap-[10px]">
+                                                    <i className={item.icon}></i>
+                                                    <p>{item.label}</p>
+                                                </div>
+                                                <p>{item.value}</p>
+                                            </div>
+                                            {index === 1 ? (
+                                                <div className="w-[100%] border-t-[1px] border-dashed border-[#000] mb-[20px]"></div>
+                                            ) : null}
+                                        </Fragment>
+                                    );
+                                })}
+                            </div>
+
+                            <button className="rounded-[10px] bg-[#ff4444] text-[#fff] px-[32px] py-[8px] font-[500]">
+                                Tiếp tục
+                            </button>
                         </div>
                     </div>
                 </ContainerWapper>
@@ -136,46 +271,51 @@ const SeatBooking = () => {
 
 export default SeatBooking;
 
-const ListChair = ({ target, booked, booking, setBooking, price }) => {
+const ListChair = ({ target, booked, booking, setBooking, price, isDoubleChair, type }) => {
     return (
-        <div className="flex justify-between items-center">
+        <div className="flex justify-center items-center gap-[50px]">
             <p className="uppercase font-[500] text-[20px]">{target}</p>
-            {[
-                { length: 4, space: 1 },
-                { length: 13, space: 6 },
-                { length: 5, space: 19 },
-            ].map((item, index) => {
-                return (
-                    <div className="flex justify-start items-center gap-[10px]" key={index}>
-                        {Array.from({ length: item.length }, (_, i) => `${target}${i + item.space}`).map((item) => {
-                            return (
-                                <Chair
-                                    name={item}
-                                    key={item}
-                                    booked={booked}
-                                    booking={booking}
-                                    setBooking={setBooking}
-                                    price={formatVND(price)}
-                                />
-                            );
-                        })}
-                    </div>
-                );
-            })}
+            <div className="flex justify-start items-center gap-[10px]">
+                {Array.from({ length: isDoubleChair ? 9 : 18 }, (_, i) => `${target}${i}`).map((item) => {
+                    return (
+                        <Chair
+                            name={item}
+                            key={item}
+                            booked={booked}
+                            booking={booking}
+                            setBooking={setBooking}
+                            price={formatVND(price)}
+                            isDoubleChair={isDoubleChair}
+                            type={type}
+                        />
+                    );
+                })}
+            </div>
         </div>
     );
 };
 
-const Chair = ({ name, booked, booking, setBooking, price }) => {
+const Chair = ({ name, booked, booking, setBooking, price, isDoubleChair, type }) => {
     const isBooked = useMemo(() => booked.includes(name), [name, booked]);
     const isBooking = useMemo(() => booking.includes(name), [name, booking]);
 
     const color = useMemo(() => {
         if (booked.includes(name)) {
-            return '#4d4f5c';
+            return '#babac1';
         }
         if (booking.includes(name)) {
-            return '#f3c600';
+            return '#1d59a2';
+        }
+
+        if (type === 'nomal') {
+            return '#7331d6';
+        }
+        if (type === 'vip') {
+            return '#f14052';
+        }
+
+        if (isDoubleChair) {
+            return '#dd3998';
         }
         return '';
     }, [booked, booking, name]);
@@ -195,17 +335,16 @@ const Chair = ({ name, booked, booking, setBooking, price }) => {
                 style={{ cursor: isBooked ? 'not-allowed' : 'pointer' }}
             >
                 <div
-                    className="w-[40px] h-[25px] rounded-[5px] border-[1px] border-solid border-[#4d4f5c] flex justify-center items-center"
-                    style={{ background: color, borderColor: color, color: isBooking ? '#000' : '' }}
+                    className="w-[40px] h-[40px] rounded-[5px] border-[1px] border-solid border-[#4d4f5c] flex justify-center items-center uppercase text-[#fff]"
+                    style={{
+                        background: color,
+                        borderColor: color,
+                        width: isDoubleChair ? '80px' : '40px',
+                    }}
                     onClick={isBooked ? null : handleChoose}
                 >
-                    {name?.slice(1)}
+                    {name}
                 </div>
-                <div
-                    className="w-[30px] h-[10px] rounded-b-[5px] rounded-t-[2px] border-[1px] border-solid border-[#4d4f5c]"
-                    style={{ background: color, borderColor: color }}
-                    onClick={isBooked ? null : handleChoose}
-                ></div>
             </div>
         </Popover>
     );
